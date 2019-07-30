792f5ae07701960e307a01039bad57e10a3e9977
Yannis <Yannis182@outlook.fr>
2019-07-30 16:54:21 +0200

    minor changes

--- src/landingpage/LandingPage.js
+++ src/landingpage/LandingPage.js
@@ -27,7 +27,7 @@ const CustomForm = ({ status, message, onValidated }) => {
         <div
         className="success-message"
           style={{ color: "green"}}
-          dangerouslySetInnerHTML={{ __html: message }}
+       dangerouslySetInnerHTML={{ __html: "Merci !" }}
         />
       )}
 
@@ -56,7 +56,7 @@ class Form extends Component {
 
         <p className="main-text">
           {" "}
-          Tu es passionné par l’univers de la blockchain et des cryptomonnaies <br /> et tu souhaites en faire ton métier ?<br /> Inscris toi à notre newsletter pour suivre  le lancement d' OnChainJobs <br /> et recevoir nos articles en avant premières!
+          Tu es passionné par l’univers de la blockchain et des cryptomonnaies <br /> et tu souhaites en faire ton métier ?<br /> Inscris-toi à notre newsletter pour suivre le lancement d'OnChainJobs <br /> et recevoir nos articles en avant-premières !
         </p>
         <MailchimpSubscribe
           url={url}
